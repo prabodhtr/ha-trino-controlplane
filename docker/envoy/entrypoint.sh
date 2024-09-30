afeee1f2d3e51b8b362503fd001b198b17c5c785
prabodhtr <prabodhtr@gmail.com>
2024-10-01 00:52:31 +0530

    update envoy docker entrypoint script

--- docker/envoy/entrypoint.sh
+++ docker/envoy/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Generating envoy_final.yaml config file..."
-cat /etc/envoy/envoy.yaml | envsubst \$CONTROLPLANE_XDS_PORT,\$CONTROLPLANE_CACHE_PORT,\$ENVOY_ADMIN_PORT > /etc/envoy/envoy_final.yaml
+echo "Generating envoy_config.yaml file..."
+cat /etc/envoy/envoy.yaml | envsubst \$CONTROLPLANE_XDS_PORT,\$CONTROLPLANE_CACHE_PORT,\$ENVOY_ADMIN_PORT > /envoy_config.yaml
 
 echo "Starting Envoy..."
-/usr/local/bin/envoy -c /etc/envoy/envoy_final.yaml
+/usr/local/bin/envoy -c /envoy_config.yaml
